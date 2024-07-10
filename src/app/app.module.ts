f409e137ef58a3ebace84cd56df2208f51225793
Minal <minalvaghela1307@gmail.com>
2024-07-10 19:55:24 +0200

    1st commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms'
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrl: './todolist.component.css'
+})
+export class TodolistComponent implements OnInit {
+
+  taskArray = [{taskName: 'To Do Data-1', isCompleted: false}];
+  constructor(){}
+
+  ngOnInit(): void{
+
+  }
+
+  onSubmit(form : NgForm){
+    console.log(form);
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
+    form.reset();
+  }
+
+  onDelete(index: number){
+    console.log(index);
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number){
+    console.log(this.taskArray);
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+}
